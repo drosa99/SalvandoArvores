5aaf20b0cc3117331740627226ad1770ba359f23
Daniela <danielarosa2309@gmail.com>
2018-09-11 12:30:03 -0300

    iniciando

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+public class Node{
+    String elemento;
+    Node esquerda, direita;
+
+    public Node (String entrada){
+        elemento = entrada;
+        esquerda = null;
+        direita = null;
+    }
+}
